a13b7d7c2f1d680e2ed11cc1ad0344cdd8b46903
Arne Bahlo <hallo@arne.me>
2013-12-06 20:10:42 +0100

    Implement basic methods for mixer

--- semester3/practice4/code/Mixer.cpp
+++ semester3/practice4/code/Mixer.cpp
@@ -2,39 +2,47 @@
 
 
 #include "Mixer.h"
+#include "Timer.h"
+
+//
+Mixer::Mixer() : isCleaned(false) {}
+
 //
 void Mixer::clean()
 {
-    
+  // Clean?
+  Timer::wait(2000);
+  isCleaned = true;
 }
 
 //
-void Mixer::stopMixing()
+void Mixer::mix(int seconds)
 {
-    
+  start();
+  Timer::wait(seconds * 1000);
+  stop();
 }
 
 //
-void Mixer::startMixing()
+void Mixer::drain()
 {
-    
+  open();
+  Timer::wait(5000); // Scale?
+  close();
 }
 
-//
-void Mixer::startDrain()
-{
-    
+void Mixer::start() {
+  // Start mixing
 }
 
-//
-void Mixer::stopDrain()
-{
-    
+void Mixer::stop() {
+  // Stop mixing
 }
 
-//
-Mixer::Mixer()
-{
-    
+void Mixer::open() {
+  // Open drain
 }
 
+void Mixer::close() {
+  // Close drain
+}
