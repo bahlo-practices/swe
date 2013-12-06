9b61b53a48aeb7d9de6357073d0ec740932e44aa
Arne Bahlo <hallo@arne.me>
2013-12-06 21:11:55 +0100

    Implement Subject.cpp

--- semester3/practice4/code/Subject.cpp
+++ semester3/practice4/code/Subject.cpp
@@ -2,20 +2,27 @@
 
 
 #include "Subject.h"
-//
-void Subject::attach(const Observer* observer)
+
+void Subject::attach(Observer* observer)
 {
-    
+  observers.push_back(observer);
 }
 
-//
 void Subject::notify()
 {
-    
+  int countObservers = observers.size();
+  for(int i = 0; i < countObservers;++i) {
+    observers.at(i)->update();
+  }
 }
 
-//
-void Subject::detach(const Observer* observer)
+void Subject::detach(Observer* observer)
 {
-    
+  int countObservers = observers.size();
+  for(int i = 0; i < countObservers;++i) {
+    if(observer == observers.at(i)) {
+      observers.erase(observers.begin() + i);
+      break;
+    }
+  }
 }
