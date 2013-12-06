5f90982efe213ed4095f3313aacf407bcd3e41e3
Christoph <christoph142@gmx.com>
2013-12-06 16:45:50 +0100

    Finish timer class

--- semester3/practice4/code/Timer.h
+++ semester3/practice4/code/Timer.h
@@ -3,11 +3,12 @@
 #ifndef TIMER_H_H
 #define TIMER_H_H
 //
-class Timer
+class Timer : public Subject
 {
 public:
     //
     static void setTurbo(bool turbo);
+    static void sleep(long milliseconds);
 private:
     //
     static bool turbo;
